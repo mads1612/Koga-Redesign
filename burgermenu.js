726e078e2c59c6e56ef6a8bda22c2e7ff4a5e809
suba0107 <suba0107@stud.kea.dk>
2019-11-01 12:45:46 +0100

    Css variabler, header og footer seperat/DRY

---
+++ burgermenu.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+    /*   document.querySelector("#menuknap").addEventListener("click", toggleMenu); Klik eventlistener på burgermenuen
+     */
+    hentNav();
+}
+
+async function hentNav() {
+    const response = await fetch("/header.html");
+    const include = await response.text();
+    document.querySelector("header").innerHTML = include;
+}
+
+/*function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#burgermenu").classList.toggle("hide"); Toggle på menuen, så den åbner og lukker, når man klikker på den*/
+/*
+    let erSkjult = document.querySelector("#burgermenu").classList.contains("hide"); Varibel, når menuen indeholder "hide" som klasse*/
+
+/* if (erSkjult == true) {
+     document.querySelector("#menuknap").textContent = "☰"; Hvis menuen indeholder classen "skjult" sæt tegnet til at være dette
+ } else {*/
+/*      document.querySelector("#menuknap").textContent = "X"; Ellers skift tegn til X
+    }
+}
+*/
