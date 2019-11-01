81b1a8f9a236c9e7168e2bc26fb972080b9849d9
suba0107 <suba0107@stud.kea.dk>
2019-11-01 13:05:14 +0100

    burgermenu fungerer ved klik (js)

--- burgermenu.js
+++ burgermenu.js
@@ -1,27 +1,39 @@
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
-    /*   document.querySelector("#menuknap").addEventListener("click", toggleMenu); Klik eventlistener på burgermenuen
-     */
+
     hentNav();
+
+    /*  Klik eventlistener på burgermenuen*/
+
 }
 
 async function hentNav() {
     const response = await fetch("/header.html");
     const include = await response.text();
     document.querySelector("header").innerHTML = include;
+    clickBurgermenu();
 }
 
-/*function toggleMenu() {
-    console.log("toggleMenu");
-    document.querySelector("#burgermenu").classList.toggle("hide"); Toggle på menuen, så den åbner og lukker, når man klikker på den*/
-/*
-    let erSkjult = document.querySelector("#burgermenu").classList.contains("hide"); Varibel, når menuen indeholder "hide" som klasse*/
+function clickBurgermenu() {
 
-/* if (erSkjult == true) {
-     document.querySelector("#menuknap").textContent = "☰"; Hvis menuen indeholder classen "skjult" sæt tegnet til at være dette
- } else {*/
-/*      document.querySelector("#menuknap").textContent = "X"; Ellers skift tegn til X
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#burgermenu").classList.toggle("hide");
+    /*Toggle på menuen, så den åbner og lukker, når man klikker på den
+     */
+    let erSkjult = document.querySelector("#burgermenu").classList.contains("hide"); /*Varibel, når menuen indeholder "hide" som klasse*/
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+        /*  Hvis menuen indeholder classen "skjult"
+  sæt tegnet til at være dette*/
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+        /*    Ellers skift tegn til X*/
     }
 }
-*/
